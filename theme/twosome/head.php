33f52cb8b28f7de4fc1eca4ca7d943eef08389f3
jungwoonhee <41932121+jungwoonhee@users.noreply.github.com>
2018-11-20 17:33:25 +0900

    head_tail_tailback

--- theme/twosome/head.php
+++ theme/twosome/head.php
@@ -32,17 +32,20 @@ include_once(G5_LIB_PATH.'/popular.lib.php');
     .side_two{position: fixed;z-index: 10;right: 50px;top: 278px;background: url(<? echo G5_THEME_IMG_URL ?>/two/side2.png);width: 146px;height: 97px;}
     .side_two a{line-height: 120px;padding-left: 12px;}
 
+    .tab:after{content: "";display: block;clear: both;}
+    .tab li{float: left;font-size: 24px}
+
 </style>
 <div class="header_bg">
     <header id="header_wrap">
-
+    
             <a href="<?php echo G5_URL ?>"><img src="<?echo G5_THEME_IMG_URL?>/two/logo.png" alt="logo"></a>
-
-
+             
+    
             <ul class="gnb">
     <?php if ($is_member) {  ?>
-
-
+    
+    
                 <li><a href="<?php echo G5_BBS_URL ?>/member_confirm.php?url=<?php echo G5_BBS_URL ?>/register_form.php"><i class="fa fa-cog" aria-hidden="true"></i> 정보수정</a></li>
                 <li><a href="<?php echo G5_BBS_URL ?>/logout.php"><i class="fa fa-sign-out" aria-hidden="true"></i> 로그아웃</a></li>
                 <?php if ($is_admin) {  ?>
@@ -54,46 +57,58 @@ include_once(G5_LIB_PATH.'/popular.lib.php');
                 <?php }  ?>
                 <li>고객센터</li>
             </ul>
-             <?php  include(G5_THEME_PATH.'/skin/nav/menu.php'); ?>
-
-
-
+             <?php  include(G5_THEME_PATH.'/skin/nav/menu.php'); ?> 
+               
+                
+            
     </header>
 </div>
 
 <?php    if(defined('_INDEX_')) {     // index에서만 실행?>
 
-    <section id="recomm_book">
-    main selection
-    </section>
+    <section id="recomm_book" style="display: none">
+        
+    </section> 
 
 <?}else{?>
 
     <section id="recomm_book">
+        <?if($co_id == "B1" || $co_id == "B2" || $co_id == "B3"){?>
+        <img src="<? echo G5_THEME_IMG_URL ?>/two/sub_bg.png" alt="sub_bg">
+        <?}else if($co_id == "M1" || $co_id == "M2" || $co_id == "M3" || $co_id == "M4" ||$co_id == "M5"){?>
+        <img src="<? echo G5_THEME_IMG_URL ?>/two/notice.jpg" alt="menu_bg">
+        <?}else if($co_id == "C1" || $co_id == "C2"){?>
+        <img src="<? echo G5_THEME_IMG_URL ?>/two/class-title.png" alt="class_bg">
+        <?}else if($bo_table == "free"){?>
+        <img src="<? echo G5_THEME_IMG_URL ?>/two/store.jpg" alt="store">
+        <?}else{?>
+           <img src="<? echo G5_THEME_IMG_URL ?>/two/membership.jpg" alt="membership_bg">
+        <?}?>
+
 
     <!-- 현재위치 -->
     <span style="display:inline-block">
-    <?php
-        $sql_menu = " select *  from ".$g5['menu_table']."
-        where me_use = '1'
-        and length(me_code) = '2'
-        order by me_order, me_id ";
-        $result_menu = sql_query($sql_menu, false);
+    <?php 
+        $sql_menu = " select *  from ".$g5['menu_table']." 
+        where me_use = '1' 
+        and length(me_code) = '2' 
+        order by me_order, me_id "; 
+        $result_menu = sql_query($sql_menu, false); 
+        
 
 
-
-        for ($i=0; $rowMenu=sql_fetch_array($result_menu); $i++) {
-            $rowMenu_link = explode("=",$rowMenu['me_link']);
+        for ($i=0; $rowMenu=sql_fetch_array($result_menu); $i++) { 
+            $rowMenu_link = explode("=",$rowMenu['me_link']); 
            //echo $rowMenu_link[1];
 
-
-      ?>
-         <a href="<?php echo $rowMenu['me_link']; ?>" target="_<?php echo $rowMenu['me_target']; ?>" style="display:none" id="menu_link<?php echo $i ?>"><?php echo $rowMenu['me_name']; ?></a>
+            
+      ?>      
+         <a href="<?php echo $rowMenu['me_link']; ?>" target="_<?php echo $rowMenu['me_target']; ?>" style="display:none" id="menu_link<?php echo $i ?>"><?php echo $rowMenu['me_name']; ?></a>  
 
         <?}?>
 
     </span>
-     >
+     >   
      <? if($bo_table){echo $board['bo_subject']; }else{echo $g5['title']; }
      ?>
      <!-- 현재위치끝 -->
@@ -102,11 +117,17 @@ include_once(G5_LIB_PATH.'/popular.lib.php');
 
     <?}?>
 
-    <div id="content" style="overflow:hidden">
-        <?php
+  <div id="content" style="overflow:hidden">
+    <? if(!defined('_INDEX_')) {?>
+        <div id="aside" >
+        <?php  include(G5_THEME_PATH.'/skin/nav/mysubmenu.php'); ?> 
+        </div>
+    <?}?>
+
+        <?php 
         if(defined('_INDEX_')) {     // index에서만 실행
         ?>
             <div class="content" style="width:100%;">
         <?}else{?>
-            <div class="content" style="width:75%;float:left">
+            <div class="content" style="width:100%;margin-bottom: 100px">
         <?}?>
