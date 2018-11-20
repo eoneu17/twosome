ef71b4451f16949084fdca7088a04d7e6cbf9f91
xkzldhs10 <41932224+xkzldhs10@users.noreply.github.com>
2018-11-20 13:44:19 +0900

    Add files via upload

--- theme/twosome/index.php
+++ theme/twosome/index.php
@@ -10,38 +10,120 @@ if (G5_IS_MOBILE) {
 include_once(G5_THEME_PATH.'/head.php');
 ?>
 
-<h2 class="sound_only">최신글</h2>
 
-<div class="latest_wr">
-<!-- 최신글 시작 { -->
 
-    <?php
-    //  최신글
-    $sql = " select bo_table
-                from `{$g5['board_table']}` a left join `{$g5['group_table']}` b on (a.gr_id=b.gr_id)
-                where a.bo_device <> 'mobile' ";
-    if(!$is_admin)
-        $sql .= " and a.bo_use_cert = '' ";
-    $sql .= " and a.bo_table not in ('notice', 'gallery') ";     //공지사항과 갤러리 게시판은 제외
-    $sql .= " order by b.gr_order, a.bo_order ";
-    $result = sql_query($sql);
-    for ($i=0; $row=sql_fetch_array($result); $i++) {
-        if ($i%2==1) $lt_style = "margin-left:2%";
-        else $lt_style = "";
-    ?>
-    <div style="float:left;<?php echo $lt_style ?>" class="lt_wr">
-        <?php
-        // 이 함수가 바로 최신글을 추출하는 역할을 합니다.
-        // 사용방법 : latest(스킨, 게시판아이디, 출력라인, 글자수);
-        // 테마의 스킨을 사용하려면 theme/basic 과 같이 지정
-        echo latest('theme/basic', $row['bo_table'], 6, 24);
-        ?>
-    </div>
-    <?php
-    }
-    ?>
-    <!-- } 최신글 끝 -->
 
+
+
+
+<style>
+#slider_wrap{width: 100%;height:600px;background:blue;}
+	
+    
+	#bar1{width: 1920px;height:200px;}
+	#bar2{width: 1920px;height:600px; margin:0 auto;}
+	
+	
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+  #class-links{
+    position: relative;
+    width: 800px;
+    height: 600px;
+    margin: 20px auto;
+  }
+  #class-links a {
+    position: absolute;
+    display: block;
+    width: 500px;
+    height: 500px;
+    padding: 10px;
+    overflow: hidden;
+  }
+  #class-links img{
+    width: 100%;
+    height: 100%;
+    transition: 1s;
+  }
+
+  #class-links a:hover img{
+    transform: scale(1.2);
+  }
+
+  #class-links .coffee {
+    top: 0;left: 0;
+  }
+
+  #class-links .cake {
+    bottom: 0;right: 0;
+  }
+  #class-links p{
+    position: absolute;
+    padding: 40px;
+    color: #fff;
+    font-weight: bold;
+    font-size: 40px;
+    text-align: center;
+    z-index: 1;
+  }
+  #class-links .cake p{
+    bottom: 0;right: 0;
+  }
+</style>
+<script>
+      $(document).ready(function(){
+         $('.slider').bxSlider({
+            auto:true,
+            controls:false, 
+            pager:false
+         });
+         
+   
+
+       
+   
+   
+         });
+      
+
+
+		
+	
+</script>
+
+
+
+
+<section id="slider_wrap">
+    <div class="slider">
+		<div><img src="<?echo G5_THEME_IMG_URL?>/slider1.jpg" alt="a"></div>
+		<div><img src="<?echo G5_THEME_IMG_URL?>/slider2.jpg" alt="a"></div>
+		<div><img src="<?echo G5_THEME_IMG_URL?>/slider3.jpg" alt="a"></div>
+	</div>
+</section> 
+	
+
+<div id="bar1">
+<img src="<?echo G5_THEME_IMG_URL?>/bar1.jpg" alt="bar1">
+</div>
+
+<div id="class-links">
+    <a href="#" class="coffee">
+      <p>THE COFFEE<br>TEACHING<br>CLASS</p>
+     <img src="<?echo G5_THEME_IMG_URL?>/bg1.png" alt="bg1">
+    </a>
+    <a href="#" class="cake">
+      <p>THE CAKE<br>TEACHING<br>CLASS</p>
+    <img src="<?echo G5_THEME_IMG_URL?>/bg2.png" alt="bg2">
+    </a>
+  </div>
+
+<div id="bar2">
+
+	<img src="<?echo G5_THEME_IMG_URL?>/bar2.jpg" alt="bar2">
 </div>
 
 <div class="latest_wr">
@@ -50,7 +132,7 @@ include_once(G5_THEME_PATH.'/head.php');
     // 이 함수가 바로 최신글을 추출하는 역할을 합니다.
     // 사용방법 : latest(스킨, 게시판아이디, 출력라인, 글자수);
     // 테마의 스킨을 사용하려면 theme/basic 과 같이 지정
-    echo latest('theme/pic_basic', 'gallery', 5, 23);
+ 
     ?>
     <!-- } 사진 최신글2 끝 -->
 </div>
